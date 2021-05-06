060c3ea349da6464562b0234d21e9a26f7a8dcc0
Arthur Borges <arthurborges.dev@gmail.com>
2021-05-06 20:07:14 -0300

    Remove comments from the posts_controller file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,23 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
   before_action :authenticate_user!, only: %i[new create]
-  # GET /posts or /posts.json
+
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
+
   def show; end
 
-  # GET /posts/new
+
   def new
     @post = current_user.posts.build
   end
 
-  # GET /posts/1/edit
+
   def edit; end
 
-  # POST /posts or /posts.json
+
   def create
     @post = current_user.posts.new(post_params)
 
@@ -32,7 +32,7 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
+
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -45,7 +45,7 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
+
   def destroy
     @post.destroy
     respond_to do |format|
@@ -56,12 +56,12 @@ class PostsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
+
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
+
   def post_params
     params.require(:post).permit(:title, :body)
   end
