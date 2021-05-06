00b16364dfa23a3561d44a9aab2ea216759068f5
jmontanholi <jonunes9326@hotmail.com.br>
2021-05-06 17:37:47 -0300

    Finish assigning author to posts when signed in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_194833) do
+ActiveRecord::Schema.define(version: 2021_05_06_202327) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -28,7 +28,9 @@ ActiveRecord::Schema.define(version: 2021_05_06_194833) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
