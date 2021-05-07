b790fc620845a78cf18c54bc4f0b3c5835792d27
jmontanholi <jonunes9326@hotmail.com.br>
2021-05-07 16:46:35 -0300

    Start navbar styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,8 @@ module ApplicationHelper
       res << "Logged in as <strong> #{current_user.name} </strong>"
       res << (link_to 'Log out', destroy_user_session_path, method: :delete)
     else
-      res << (link_to 'Sign in', user_session_path)
-      res << (link_to 'Sign Up', new_user_registration_path)
+      res << (link_to 'Sign in', user_session_path, class:"text-light mx-4 link p-2")
+      res << (link_to 'Sign Up', new_user_registration_path, class:"text-light mx-4 link p-2")
     end
     res.html_safe
   end
