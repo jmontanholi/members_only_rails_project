c8937cc68f1ada1d80111cc22ccc130e1c4eaf50
jmontanholi <jonunes9326@hotmail.com.br>
2021-05-07 18:44:10 -0300

    Finish index page styling and navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
   def check_user
     res = ''
     if user_signed_in?
-      res << "Logged in as <strong> #{current_user.name} </strong>"
-      res << (link_to 'Log out', destroy_user_session_path, method: :delete)
+      res << "Logged in as <strong> #{current_user.name.capitalize} </strong>"
+      res << (link_to 'Log out', destroy_user_session_path, method: :delete, class:"link bg-danger rounded text-light p-2 ms-2")
     else
       res << (link_to 'Sign in', user_session_path, class:"text-light mx-4 link p-2")
       res << (link_to 'Sign Up', new_user_registration_path, class:"text-light mx-4 link p-2")
